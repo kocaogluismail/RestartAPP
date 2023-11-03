c48e7bab44d875b00768ab302b1155c320755a3d
İsmail Kocaoğlu <100610519+kocaogluismail@users.noreply.github.com>
2023-11-03 21:32:19 +0300

    RestartAPP
    if you wanna have a quiet time, you can use this application.

---
+++ Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,23 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by İsmail Kocaoglu on 3.11.2023.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer : AVAudioPlayer?
+
+func playSound(sound: String, type : String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf:URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file")
+        }
+    }
+}
+
